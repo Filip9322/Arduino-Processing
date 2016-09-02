a9cb38371bc61ef0a61a0c3a5fbbfe2cadafb97e
filip9322 <filip9322@gmail.com>
2016-09-02 00:46:23 -0500

    Se logra el codigo del Arduino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,9 +1,15 @@
 char val;
-int ledPin = 13;
+int ledStrip1 = 8;
+int ledStrip2 = 9;
+int ledStrip3 = 10;
+int ledStrip4 = 11;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledStrip1, OUTPUT);
+  pinMode(ledStrip2, OUTPUT);
+  pinMode(ledStrip3, OUTPUT);
+  pinMode(ledStrip4, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -11,11 +17,26 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available()) {
     val = Serial.read();
-  }
-  if (val == '1') {
-    digitalWrite(ledPin, HIGH);
-  } else {
-    digitalWrite(ledPin, LOW);
+  } if (val == '1') {
+    digitalWrite(ledStrip1, HIGH);
+    digitalWrite(ledStrip2, LOW);
+    digitalWrite(ledStrip3, LOW);
+    digitalWrite(ledStrip4, LOW);
+  } else if (val == '2') {
+    digitalWrite(ledStrip1, HIGH);
+    digitalWrite(ledStrip2, HIGH);
+    digitalWrite(ledStrip3, LOW);
+    digitalWrite(ledStrip4, LOW);
+  } else if (val == '3') {
+    digitalWrite(ledStrip1, HIGH);
+    digitalWrite(ledStrip2, HIGH);
+    digitalWrite(ledStrip3, HIGH);
+    digitalWrite(ledStrip4, LOW);
+  } else if (val == '4') {
+    digitalWrite(ledStrip1, HIGH);
+    digitalWrite(ledStrip2, HIGH);
+    digitalWrite(ledStrip3, HIGH);
+    digitalWrite(ledStrip4, HIGH);
   }
   delay(10);
 }
